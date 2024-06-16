cf2bbe6c9bd29c74ea514115438225c170cf695b
Benjamin Harris <113062337+hbjamin@users.noreply.github.com>
2024-06-16 21:04:02 +0200

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,19 @@
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+let dictionary = [];
+let word = '';
+let guesses = [];
+let check = false;
+let stats = false;
+
+let canvasX = 400;
+let canvasY = 600;
+
+let backgroundColor;
+let textColor;
+
+
+let keyboard = [];
+row1 = ['q','w','e','r','t','y','u','i','o','p'];
+row2 = ['a','s','d','f','g','h','j','k','l'];
+row3 = ['z','x','c','v','b','n','m'];
